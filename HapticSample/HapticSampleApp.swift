fddd6a9026ef4a4cc9277ee8110e290f9480d9cf
WILLIAM JONATHAN HANDOKO <108049958+WillieHandoko@users.noreply.github.com>
2024-10-29 16:16:21 +0700

    Initial Commit

---
+++ HapticSample/HapticSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticSampleApp.swift
+//  HapticSample
+//
+//  Created by William Handoko on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HapticSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
